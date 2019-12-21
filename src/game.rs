a138de249d334cee9093a6039025d580178d0487
Alexander Johnston <Aliics@hotmail.com>
2019-12-22 12:40:01 +1300

    Add messy implementation for paddle movement

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,6 @@
-use crate::paddle::PADDLE_START;
+use crate::paddle::{PADDLE_SPEED, PADDLE_START};
 use piston_window::{
-    clear, rectangle, Button, ButtonArgs, ButtonEvent, ButtonState, Event, PistonWindow,
-    RenderEvent,
+    clear, rectangle, Button, ButtonEvent, ButtonState, Event, Key, PistonWindow, RenderEvent,
 };
 
 const BACKGROUND_COLOR: [f32; 4] = [0.0; 4];
@@ -10,6 +9,7 @@ const FOREGROUND_COLOR: [f32; 4] = [1.0; 4];
 pub struct Game {
     window: PistonWindow,
     paddle_transforms: [[f64; 4]; 2],
+    paddle_movements: [[f64; 2]; 2],
 }
 
 impl Game {
@@ -17,6 +17,7 @@ impl Game {
         Game {
             window,
             paddle_transforms: PADDLE_START,
+            paddle_movements: [[0.0; 2]; 2],
         }
     }
 
@@ -26,15 +27,23 @@ impl Game {
                 self.render(&e)
             }
             if let Some(b) = e.button_args() {
-                match b.state {
-                    ButtonState::Press => self.key_down(b.button),
-                    ButtonState::Release => self.key_up(b.button),
+                if let Button::Keyboard(k) = b.button {
+                    match b.state {
+                        ButtonState::Press => self.key_down(k),
+                        ButtonState::Release => self.key_up(k),
+                    }
                 }
             }
         }
     }
 
+    fn calc_position(&mut self) {
+        self.paddle_transforms[0][1] -= self.paddle_movements[0][0];
+        self.paddle_transforms[0][1] += self.paddle_movements[0][1];
+    }
+
     fn render(&mut self, event: &Event) {
+        self.calc_position();
         let paddle_slices = self.paddle_transforms.clone();
         self.window.draw_2d(event, |c, g, _| {
             clear(BACKGROUND_COLOR, g);
@@ -44,11 +53,21 @@ impl Game {
         });
     }
 
-    fn key_down(&mut self, button: Button) {
-        println!("button down {:?}", button);
+    fn key_down(&mut self, key: Key) {
+        if key == Key::A {
+            self.paddle_movements[0][0] = PADDLE_SPEED
+        }
+        if key == Key::D {
+            self.paddle_movements[0][1] = PADDLE_SPEED
+        }
     }
 
-    fn key_up(&mut self, button: Button) {
-        println!("button up {:?}", button);
+    fn key_up(&mut self, key: Key) {
+        if key == Key::A {
+            self.paddle_movements[0][0] = 0.0
+        }
+        if key == Key::D {
+            self.paddle_movements[0][1] = 0.0
+        }
     }
 }
--- src/paddle.rs
+++ src/paddle.rs
@@ -2,6 +2,8 @@ use crate::window::WINDOW_SIZE;
 
 const PADDLE_SIZE: [f64; 2] = [10.0, 100.0];
 
+pub const PADDLE_SPEED: f64 = 5.0;
+
 pub const PADDLE_START: [[f64; 4]; 2] = [
     [0.0, 0.0, PADDLE_SIZE[0], PADDLE_SIZE[1]],
     [
